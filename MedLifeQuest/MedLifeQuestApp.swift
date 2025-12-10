da1c553b7397299d8588ec7b17484f0094cf8a1d
ludik <bakhanetssimon@gmail.com>
2025-12-10 19:53:45 +0300

    Initial Commit

---
+++ MedLifeQuest/MedLifeQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedLifeQuestApp.swift
+//  MedLifeQuest
+//
+//  Created by Simon Bakhanets on 10.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MedLifeQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
